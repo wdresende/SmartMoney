0636214b93ee753b52e5d18b9338f99cbecfb9e6
Diego Resende <wdresende@gmail.com>
2020-06-03 01:09:09 -0300

    create the new name and id of the new application

--- android/app/src/main/java/br/com/diegoresende/smartmoney_course/MainActivity.java
+++ android/app/src/main/java/br/com/diegoresende/smartmoney_course/MainActivity.java
@@ -1,4 +1,4 @@
-package com.smartmoney;
+package br.com.diegoresende.smartmoney_course;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "smartMoney";
+    return "SmartMoney";
   }
 }
